eafbc093ca47205267859eb1b7077b3b61066faf
Ronnie <unstablesort@gmail.com>
2019-08-29 11:01:51 -0500

    refactor(static mediator): defer generic types to dynamic registration

--- Turner.Infrastructure.Mediator.Tests/StaticMediatorPipelineTests.cs
+++ Turner.Infrastructure.Mediator.Tests/StaticMediatorPipelineTests.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using Shouldly;
 using SimpleInjector;
-using System.Reflection;
-using System.Threading.Tasks;
 using Turner.Infrastructure.Mediator.Configuration;
+using Turner.Infrastructure.Mediator.Decorators;
 using Turner.Infrastructure.Mediator.Tests.Fakes;
 
 namespace Turner.Infrastructure.Mediator.Tests
@@ -24,6 +25,9 @@ namespace Turner.Infrastructure.Mediator.Tests
             {
                 GetType().GetTypeInfo().Assembly
             });
+
+            Container.Register(typeof(IRequestHandler<>), typeof(GenericHandler<>));
+            Container.Register(typeof(IRequestHandler<,>), typeof(GenericHandlerWithResult<>));
 
             Container.RegisterSingleton<DbContext>(() => new FakeDbContext());
         }
@@ -47,6 +51,119 @@ namespace Turner.Infrastructure.Mediator.Tests
             // Assert
             response.HasErrors.ShouldBe(false);
             response.Data.ShouldBe("Bar");
+        }
+
+        [Test]
+        public async Task Handle_WithGenericBaseRequestHandlers_HandleRequests()
+        {
+            // Act
+            var responseA = await Mediator.HandleAsync(new RequestA());
+            var responseB = await Mediator.HandleAsync(new RequestB());
+
+            // Assert
+            responseA.HasErrors.ShouldBe(true);
+            responseA.Errors[0].ErrorMessage.ShouldBe("RequestA");
+            responseB.HasErrors.ShouldBe(true);
+            responseB.Errors[0].ErrorMessage.ShouldBe("RequestB");
+        }
+
+        [Test]
+        public async Task Handle_WithGenericBaseWithResultRequestHandlers_HandleRequests()
+        {
+            // Act
+            var responseA = await Mediator.HandleAsync(new RequestWithResultA());
+            var responseB = await Mediator.HandleAsync(new RequestWithResultB());
+
+            // Assert
+            responseA.HasErrors.ShouldBe(true);
+            responseA.Errors[0].ErrorMessage.ShouldBe("RequestWithResultA, System.String");
+            responseB.HasErrors.ShouldBe(true);
+            responseB.Errors[0].ErrorMessage.ShouldBe("RequestWithResultB, System.String");
+        }
+
+        [Test]
+        public async Task Handle_WithGenericRequestHandlers_HandleRequests()
+        {
+            // Act
+            var responseA = await Mediator.HandleAsync(new GenericRequest<FakeEntityA>());
+            var responseB = await Mediator.HandleAsync(new GenericRequest<FakeEntityB>());
+
+            // Assert
+            responseA.HasErrors.ShouldBe(true);
+            responseA.Errors[0].ErrorMessage.ShouldBe("FakeEntityA");
+            responseB.HasErrors.ShouldBe(true);
+            responseB.Errors[0].ErrorMessage.ShouldBe("FakeEntityB");
+        }
+
+        [Test]
+        public async Task Handle_WithGenericRequestWithResultHandlers_HandleRequests()
+        {
+            // Act
+            var responseA = await Mediator.HandleAsync(new GenericRequestWithResult<FakeEntityA>());
+            var responseB = await Mediator.HandleAsync(new GenericRequestWithResult<FakeEntityB>());
+
+            // Assert
+            responseA.HasErrors.ShouldBe(true);
+            responseA.Errors[0].ErrorMessage.ShouldBe("FakeEntityA, FakeEntityA");
+            responseB.HasErrors.ShouldBe(true);
+            responseB.Errors[0].ErrorMessage.ShouldBe("FakeEntityB, FakeEntityB");
+        }
+
+
+        public class GenericBaseHandler<TRequest> : IRequestHandler<TRequest>
+            where TRequest : IRequest
+        {
+            public Task<Response> HandleAsync(TRequest request) 
+                => Error.AsResponseAsync(typeof(TRequest).Name);
+        }
+
+        public class GenericBaseHandlerWithResult<TRequest, TResult> : IRequestHandler<TRequest, TResult>
+            where TRequest : IRequest<TResult>
+        {
+            public Task<Response<TResult>> HandleAsync(TRequest request)
+                => Error.AsResponseAsync<TResult>($"{typeof(TRequest).Name}, {typeof(TResult)}");
+        }
+
+        [DoNotValidate]
+        public class RequestA : IRequest { }
+
+        [DoNotValidate]
+        public class RequestWithResultA : IRequest<string> { }
+
+        [DoNotValidate]
+        public class RequestB : IRequest { }
+
+        [DoNotValidate]
+        public class RequestWithResultB : IRequest<string> { }
+
+        public class ChildHandlerA : GenericBaseHandler<RequestA> { }
+
+        public class ChildHandlerWithResultA : GenericBaseHandlerWithResult<RequestWithResultA, string> { }
+
+        public class ChildHandlerB : GenericBaseHandler<RequestB> { }
+
+        public class ChildHandlerWithResultB : GenericBaseHandlerWithResult<RequestWithResultB, string> { }
+
+        public class FakeEntityA { }
+
+        public class FakeEntityB { }
+
+        [DoNotValidate]
+        public class GenericRequest<T> : IRequest { }
+
+        public class GenericHandler<T> : IRequestHandler<GenericRequest<T>>
+        {
+            public Task<Response> HandleAsync(GenericRequest<T> request) 
+                => Error.AsResponseAsync(typeof(T).Name);
+        }
+
+        [DoNotValidate]
+        public class GenericRequestWithResult<T> : IRequest<T> { }
+
+        public class GenericHandlerWithResult<T> : IRequestHandler<GenericRequestWithResult<T>, T>
+        {
+            public Task<Response<T>> HandleAsync(GenericRequestWithResult<T> request) 
+                => Error.AsResponseAsync<T>($"{typeof(T).Name}, {typeof(T).Name}");
         }
     }
 }
--- Turner.Infrastructure.Mediator/Mediators/StaticDispatcher.cs
+++ Turner.Infrastructure.Mediator/Mediators/StaticDispatcher.cs
@@ -8,19 +8,25 @@ namespace Turner.Infrastructure.Mediator
 {
     public class StaticDispatcher : IStaticDispatcher
     {
-        private Dictionary<Type, Func<object, object>> _dispatchers;
-
+        private readonly Func<Type, object> _handlerFactory;
+        private readonly Dictionary<Type, Func<object, object>> _dispatchers;
+          
         public StaticDispatcher(Func<Type, object> handlerFactory, params Assembly[] assemblies)
         {
+            _handlerFactory = handlerFactory ?? throw new ArgumentNullException(nameof(handlerFactory));
+            _dispatchers = new Dictionary<Type, Func<object, object>>();
+
             Register(handlerFactory, assemblies);
         }
 
         public object Dispatch(object request) =>
-            _dispatchers[request.GetType()](request);
+            _dispatchers.ContainsKey(request.GetType()) 
+                ? _dispatchers[request.GetType()](request)
+                : Register(request.GetType())(request);
 
         private void Register(Func<Type, object> handlerFactory, IEnumerable<Assembly> assemblies)
         {
-            _dispatchers = new Dictionary<Type, Func<object, object>>();
+            _dispatchers.Clear();
 
             var classTypes = assemblies
                 .SelectMany(x => x.GetTypes())
@@ -30,20 +36,22 @@ namespace Turner.Infrastructure.Mediator
             var requestTypesWithoutResult = classTypes
                 .SelectMany(type => type.GetInterfaces()
                     .Where(t => t.IsGenericType && typeof(IRequestHandler<>) == t.GetGenericTypeDefinition()))
-                .Select(type => type.GenericTypeArguments);
+                .Select(type => type.GenericTypeArguments)
+                .Where(types => types.All(t => !t.IsGenericParameter && !t.IsGenericType));
 
             foreach (var types in requestTypesWithoutResult)
             {
                 var requestType = types[0];
                 var handlerType = typeof(IRequestHandler<>).MakeGenericType(requestType);
 
-                _dispatchers[requestType] = CreateDispatcher(handlerType, requestType, handlerFactory);
+                _dispatchers[requestType] = CreateDispatcher(handlerType, requestType);
             }
 
             var requestTypesWithResult = classTypes
                 .SelectMany(type => type.GetInterfaces()
                     .Where(t => t.IsGenericType && typeof(IRequestHandler<,>) == t.GetGenericTypeDefinition()))
-                .Select(type => type.GenericTypeArguments);
+                .Select(type => type.GenericTypeArguments)
+                .Where(types => types.All(t => !t.IsGenericParameter && !t.IsGenericType));
 
             foreach (var types in requestTypesWithResult)
             {
@@ -51,18 +59,40 @@ namespace Turner.Infrastructure.Mediator
                 var resultType = types[1];
                 var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, resultType);
 
-                _dispatchers[requestType] = CreateDispatcher(handlerType, requestType, handlerFactory);
+                _dispatchers[requestType] = CreateDispatcher(handlerType, requestType);
             }
         }
 
-        private static Func<object, object> CreateDispatcher(Type handlerType, Type requestType, Func<Type, object> handlerFactory)
+        private Func<object, object> Register(Type requestType)
+        {
+            Func<object, object> dispatcher = null;
+
+            if (requestType.GetInterfaces().Any(t => t == typeof(IRequest)))
+            {
+                var handlerType = typeof(IRequestHandler<>).MakeGenericType(requestType);
+                dispatcher = CreateDispatcher(handlerType, requestType);
+            }
+            else
+            {
+                var requestInterface = requestType.GetInterfaces().FirstOrDefault(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IRequest<>))
+                    ?? throw new ArgumentException($"{requestType} does not implement IRequest or IRequest<>");
+
+                var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, requestInterface.GetGenericArguments()[0]);
+                dispatcher = CreateDispatcher(handlerType, requestType);
+            }
+
+            _dispatchers[requestType] = dispatcher;
+            return dispatcher;
+        }
+
+        private Func<object, object> CreateDispatcher(Type handlerType, Type requestType)
         {
             var handleAsyncMethod = handlerType.GetMethod("HandleAsync", new[] { requestType })
                 ?? throw new NotImplementedException();
 
-            var factoryTargetExpr = Expr.Constant(handlerFactory.Target);
+            var factoryTargetExpr = Expr.Constant(_handlerFactory.Target);
             var handlerTypeExpr = Expr.Constant(handlerType);
-            var boxedHandlerExpr = Expr.Call(factoryTargetExpr, handlerFactory.Method, handlerTypeExpr);
+            var boxedHandlerExpr = Expr.Call(factoryTargetExpr, _handlerFactory.Method, handlerTypeExpr);
             var boxedRequestExpr = Expr.Parameter(typeof(object));
             var handlerExpr = Expr.Convert(boxedHandlerExpr, handlerType);
             var requestExpr = Expr.Convert(boxedRequestExpr, requestType);
